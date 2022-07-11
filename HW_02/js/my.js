e884cbe2da822e86d62f3ca0e3b3f69b6923a6de
Liza Volyanskaya <you@example.com>
2022-07-11 21:13:35 +0300

    :Init Message

---
+++ HW_02/js/my.js
@@ -0,0 +1,11 @@
+let firstName = prompt ("Enter Your  first name");
+
+let lastName = prompt ("Enter Your  last name");
+
+let yearOfBirth = prompt ("Enter Your year of birth  ");
+
+const year = Number ("2022");
+
+let age = year  - yearOfBirth;
+
+document.write( "User Bio:" + " " + firstName + " "  + lastName + "," + " " + age + " years old"  );
