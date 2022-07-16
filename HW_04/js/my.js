7ef48ee0b7cdcb9d85090fe38e233cb6023a2765
Liza Volyanskaya <you@example.com>
2022-07-16 13:21:58 +0300

    Next_commit2

--- HW_04/js/my.js
+++ HW_04/js/my.js
@@ -1,13 +1,36 @@
 const string = prompt ("Введите произвольную строку");
 const ID = Number (prompt ("ID"));
-let a = prompt ("Unicod  якого символу треба вивести? "); alert (string.charCodeAt (a));
 
-let b =prompt ("Який символ видалити? ") ;alert (string.slice(0, b-1) + string.slice (b, string.length));
-arr = [":-)", "D)", ":)", ";)", ":P", ":]", ":-D", ":-(", ";-)", ";-P",  ];
-let smileIndex = getRandomIntInclusive(0, 9);
-let c = prompt ("Який символ замінити на смайлик? ") ; alert (string.replace ( string[c], arr[smileIndex]));
+
+if (ID == "1") {
+ let a = prompt ("Unicod  якого символу треба вивести? ")
+ alert (string.charCodeAt (a));
+}
+
+
+
+ 
+if (ID == "2"){
+let b =prompt ("Який символ видалити? ") 
+alert (string.slice(0, b-1) + string.slice (b, string.length));
+}
+
+
+if (ID == "3"){
+let c = prompt ("Який символ замінити на смайлик? ") 
+    arr = [":-)", "D)", ":)", ";)", ":P", ":]", ":-D", ":-(", ";-)", ";-P",  ];
+    let smileIndex = getRandomIntInclusive(0, 9);
+
+alert (string.replace ( string[c], arr[smileIndex]));
+}
+
+
+if (ID == "4"){
 let string1 = string.replace(/\s/g, '')
 alert (string1.length);
+}
+
+
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
