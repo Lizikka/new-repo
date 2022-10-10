4456e3483ee80f09612eaa08cd1afb1ad25044a5
Liza Volyanskaya <you@example.com>
2022-10-10 21:10:55 +0300

    Commit HW_23

---
+++ HW_23/js/my.js
@@ -0,0 +1,61 @@
+const API =  `https://61c9d37520ac1c0017ed8eac.mockapi.io/heroes`
+
+const controller = async (url, method=`GET`, obj) => {
+    let options = {
+        method: method,
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    if(obj) options.body = JSON.stringify(obj);
+
+    let request = await fetch(url, options),
+        response = request.ok ? request.json() : Promise.reject();
+
+    return response;
+}
+
+
+
+let formHeroes = document.querySelector(`#formHeroes`)
+let tableHeroes = document.createElement(`table`)
+const renderTable = () => {
+    
+    tableHeroes.innerHTML = (`<thead>
+                                    <tr>
+                                    <th>Name</th>
+                                    <th>Comics</th>
+                                    <th>Favourite</th>
+                                    <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>`)
+    formHeroes.append(tableHeroes)
+}
+
+renderTable()
+
+
+
+
+const renderHeroes = task => {
+    let tr = document.querySelector(`tbody`);
+    
+    tr.innerHTML = `<td>${task.name}</td>
+    <td>${task.comics}</td>
+    <td>${task.favourite}</td>`;
+    
+    tableHeroes.append(tr);
+    
+}
+
+const renderStorageHeroes = async () => {
+    let tasks = await controller(API);
+    tasks.map(task => renderHeroes(task));
+}
+renderStorageHeroes();
+
+
+
+
